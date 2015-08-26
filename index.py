74a4a7408c46b7ae5fb23095043100fa8d69091e
Xander <a.balette@gmail.com>
2015-08-25 23:23:10 -0400

    Added redirect and post handling for form submission.

--- index.py
+++ index.py
@@ -12,23 +12,18 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
+
+    def do_POST(s):
+        print "Posting"
+        s.do_GET()
+
     def do_GET(s):
         """Respond to a GET request."""
-        # s.send_response(200)
-        # s.send_header("Content-type", "text/html")
-        # s.end_headers()
-        # mainPage = open("index.html", "rb")
-        # content = mainPage.read()
-        # s.wfile.write(content)
-        # # s.wfile.write("<html><head><title>Title goes here.</title></head>")
-        # # s.wfile.write("<body><p>This is a test.</p>")
-        # # # If someone went to "http://something.somewhere.net/foo/bar/",
-        # # # then s.path equals "/foo/bar/".
-        # # s.wfile.write("<p>You accessed path: %s</p>" % s.path)
-        # # s.wfile.write("</body></html>")
 
         if s.path=="/":
             s.path="index.html"
+        elif s.path=="/SampleRedir":
+            s.path="SampleRedir.html"
 
         try:
             #Check the file extension required and
