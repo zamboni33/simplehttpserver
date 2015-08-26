622f2f0ae5fc6c413ec24be058987e530d3447b7
Xander <a.balette@gmail.com>
2015-08-25 22:31:43 -0400

    Added support for png images. Errors when finding files.

--- index.py
+++ index.py
@@ -13,21 +13,59 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.end_headers()
     def do_GET(s):
         """Respond to a GET request."""
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
-        mainPage = open("index.html", "rb")
-        content = mainPage.read()
-        s.wfile.write(content)
-        # s.wfile.write("<html><head><title>Title goes here.</title></head>")
-        # s.wfile.write("<body><p>This is a test.</p>")
-        # # If someone went to "http://something.somewhere.net/foo/bar/",
-        # # then s.path equals "/foo/bar/".
-        # s.wfile.write("<p>You accessed path: %s</p>" % s.path)
-        # s.wfile.write("</body></html>")
+        # s.send_response(200)
+        # s.send_header("Content-type", "text/html")
+        # s.end_headers()
+        # mainPage = open("index.html", "rb")
+        # content = mainPage.read()
+        # s.wfile.write(content)
+        # # s.wfile.write("<html><head><title>Title goes here.</title></head>")
+        # # s.wfile.write("<body><p>This is a test.</p>")
+        # # # If someone went to "http://something.somewhere.net/foo/bar/",
+        # # # then s.path equals "/foo/bar/".
+        # # s.wfile.write("<p>You accessed path: %s</p>" % s.path)
+        # # s.wfile.write("</body></html>")
+
+        if s.path=="/":
+            s.path="index.html"
+
+        try:
+            #Check the file extension required and
+            #set the right mime type
+
+            sendReply = False
+            if s.path.endswith(".html"):
+                mimetype='text/html'
+                sendReply = True
+            if s.path.endswith(".jpg"):
+                mimetype='image/jpg'
+                sendReply = True
+            if s.path.endswith(".png"):
+                mimetype='image/png'
+                sendReply = True
+            if s.path.endswith(".js"):
+                mimetype='application/javascript'
+                sendReply = True
+            if s.path.endswith(".css"):
+                mimetype='text/css'
+                sendReply = True
+
+            if sendReply == True:
+                #Open the static file requested and send it
+                f = open(s.path) 
+                s.send_response(200)
+                s.send_header('Content-type',mimetype)
+                s.end_headers()
+                s.wfile.write(f.read())
+                f.close()
+            return
+
+        except IOError:
+            s.send_error(404,'File Not Found: %s' % s.path)
 
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
+    server_class.allow_reuse_address = True
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
     print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
     try:
